694d4696ee8537240416558c9478872355172fac
dq193 <daniyalqadri.dq@gmail.com>
2023-12-26 19:04:13 +0500

    changed register button width; added navbar opacity when scroll; switched to a listener for hamburger icon

--- scripts/bannerfade.js
+++ scripts/bannerfade.js
@@ -1,5 +1,6 @@
 document.addEventListener('scroll', (ev) => {
+    // Fade banner
     const banner = document.getElementById('banner');
     const opacity = Math.max(0, 1 - 2 * (window.scrollY / window.innerHeight));
     banner.style.opacity = opacity;
-})
+});
--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,13 +1,42 @@
-var hamburgerOpen = false;
-
-function toggleHamburger() {
+function toggleNavbarOpacity(forceOpaque) {
     const navbar = document.getElementById("navbar");
-    const navmenu = document.getElementById("navmenu");
-    if (navmenu.style.display === "none") {
-        navmenu.style.display = "flex";
-        navbar.style.backgroundColor = "black";
+    const navgrad = document.getElementById("navgrad");
+    if (window.scrollY / window.innerHeight >= 1 || forceOpaque) {
+        if (navbar.classList.contains("bg-transparent")) {
+            navbar.classList.replace("bg-transparent", "bg-black");
+            navgrad.classList.replace("from-transparent", "from-black");
+        }
     } else {
-        navmenu.style.display = "none";
-        navbar.style.backgroundColor = "transparent";
+        if (navbar.classList.contains("bg-black")) {
+            navbar.classList.replace("bg-black", "bg-transparent");
+            navgrad.classList.replace("from-black", "from-transparent");
+        }
     }
 }
+
+window.addEventListener("DOMContentLoaded", (ev) => {
+    document.getElementById("hamburger").addEventListener("click", (evv) => {
+        const navbar = document.getElementById("navbar");
+        const navmenu = document.getElementById("navmenu");
+        if (navmenu.classList.contains("hidden")) {
+            navmenu.classList.replace("hidden", "flex");
+            toggleNavbarOpacity(true);
+        } else {
+            navmenu.classList.replace("flex", "hidden");
+            toggleNavbarOpacity(false);
+        }
+    });
+});
+
+document.addEventListener("scroll", (ev) => {
+    if (navmenu.classList.contains("hidden")) {
+        toggleNavbarOpacity(false);
+    }
+});
+
+window.addEventListener("resize", (ev) => {
+    const navmenu = document.getElementById("navmenu");
+    if (navmenu.classList.contains("hidden")) {
+        toggleNavbarOpacity(false);
+    }
+})
